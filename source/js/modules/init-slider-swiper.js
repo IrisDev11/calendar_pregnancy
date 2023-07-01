4faa5c0053ef5b4b2763bb891980cdc6e3c44f4f
Irina Isakova <94187168+IrisDev11@users.noreply.github.com>
2023-07-01 15:00:18 +0300

    fix: чинит слайдер на мобиле

--- source/js/modules/init-slider-swiper.js
+++ source/js/modules/init-slider-swiper.js
@@ -2,7 +2,6 @@ const initSlider = () => {
   // eslint-disable-next-line no-new, no-unused-vars, no-undef
   let swiper = new Swiper('.slider__week', {
     slidesPerView: 'auto',
-    slidesPerGroup: 8,
     spaceBetween: 20,
     speed: 1e3,
     initialSlide: 0,
@@ -15,11 +14,9 @@ const initSlider = () => {
     breakpoints: {
       320: {
         spaceBetween: 10,
-        slidesPerGroup: 3,
       },
       768: {
         spaceBetween: 10,
-        slidesPerGroup: 5,
       },
     },
   });
